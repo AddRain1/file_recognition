d1bc72578230830f40e7b9cc367aeac0ae7927c5
AddRain1 <77909535+AddRain1@users.noreply.github.com>
2025-03-01 00:11:24 -0800

    print confidence, low conf results don't get added

--- textract.py
+++ textract.py
@@ -12,22 +12,36 @@ def textract_parser(s3_bucket, s3_key, queries):
     )
 
     query_results = []
+    confidence = []
     query_texts = [query["Text"] for query in queries]  # store questions in order
 
+    """
+    for block in response.get('Blocks', []):
+        if block.get('BlockType') == 'QUERY_RESULT':
+            print(block)
+    """
     # look for block with QUERY_RESULT type and store the answer
     for block in response.get('Blocks', []):
         if block.get('BlockType') == 'QUERY_RESULT':
+
+            # if confidence isn't high enough, leave answer blank
+            conf_level = block.get('Confidence', 0)
             res_text = block.get('Text', "")
-            query_results.append(res_text)
+            if conf_level > 90:
+                query_results.append(res_text)
+            else:
+                query_results.append("")
+            confidence.append(conf_level)
 
     # merge answers into the original query JSON format
     for i in range(len(queries)):
         queries[i]["Answer"] = query_results[i] if i < len(query_results) else ""
+        queries[i]["Confidence"] = confidence[i] if i < len(confidence) else 0
 
     return(json.dumps(queries, indent=4))
 
 s3_bucket = "billiaitest"
-s3_key = "image.png"
+s3_key = "diploma.jpg"
 
 queries = [
         {"Text": "What is the document type?"},
